152d02330bf12830de0573726bd0f1ba830644be
Rafael Caricio <rafael@caricio.com>
2022-03-30 16:44:14 +0200

    Can encode raw buffers from gst into wave

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,67 @@
+use byteorder::{LittleEndian, ReadBytesExt};
 use gst::prelude::*;
 use gstreamer_app as gst_app;
+use hound::{WavSpec, WavWriter};
 use log::{debug, error, info, trace, warn};
+use std::fs::File;
+use std::io::{Cursor, Write};
 use std::process;
+use std::sync::{Arc, Mutex};
 
 fn main() -> eyre::Result<()> {
     pretty_env_logger::init();
     gst::init()?;
-    gstrusoto::plugin_register_static()?;
-    gstrsclosedcaption::plugin_register_static()?;
-    gstrstextwrap::plugin_register_static()?;
 
     let pipeline = gst::parse_launch(
         r#"
-        souphttpsrc location="https://playertest.longtailvideo.com/adaptive/elephants_dream_v4/redundant.m3u8" ! hlsdemux name=demuxer
-
-        demuxer.src_0 ! decodebin ! cccombiner name=ccc_fr ! videoconvert ! x264enc ! video/x-h264,profile=main ! muxer.video_0
-        demuxer.src_1 ! decodebin ! audioconvert ! audioresample ! opusenc ! audio/x-opus,rate=48000,channels=2 ! muxer.audio_0
-        demuxer.src_2 ! decodebin ! audioconvert ! audioresample ! opusenc ! audio/x-opus,rate=48000,channels=2 ! muxer.audio_1
-        demuxer.src_3 ! decodebin ! audioconvert ! audioresample ! opusenc ! audio/x-opus,rate=48000,channels=2 ! muxer.audio_2
-
-        souphttpsrc location="https://playertest.longtailvideo.com/adaptive/elephants_dream_v4/media_b/french/ed.m3u8" ! hlsdemux ! subparse ! tttocea608 ! ccconverter ! closedcaption/x-cea-708,format=cc_data ! ccc_fr.caption
-
-        qtmux name=muxer ! filesink location=output_cae708_only_fr.mp4
+        uridecodebin uri=file:///Users/rafael.caricio/video.mkv name=dec dec.src_1 ! audio/x-raw !
+        audioconvert ! audiorate ! audioresample ! audio/x-raw,format=S16LE,rate=48000,channels=1 ! appsink name=sink
     "#,
     )?
         .downcast::<gst::Pipeline>()
         .unwrap();
-    pipeline.set_async_handling(true);
-
-    // souphttpsrc location="https://playertest.longtailvideo.com/adaptive/elephants_dream_v4/media_b/chinese/ed.m3u8" ! hlsdemux ! subparse ! tttocea608 ! ccconverter ! closedcaption/x-cea-708,format=cc_data ! ccc_ch.caption
-    // souphttpsrc location="https://playertest.longtailvideo.com/adaptive/elephants_dream_v4/media_b/french/ed.m3u8" ! hlsdemux ! subparse ! tttocea608 ! appsink name=sink
 
     info!("Starting pipeline...");
 
-    let demuxer = pipeline.by_name("demuxer").unwrap();
+    let demuxer = pipeline.by_name("dec").unwrap();
     demuxer.connect_pad_added(|_, pad| {
         let name = pad.name();
         let caps = pad.caps().unwrap();
         let caps_type = caps.structure(0).unwrap().name();
-        // dbg!(name);
         debug!("Pad {} added with caps {}", name, caps_type);
     });
-    // let app_sink = pipeline
-    //     .by_name("sink")
-    //     .unwrap()
-    //     .downcast::<gst_app::AppSink>()
-    //     .unwrap();
-    // app_sink.set_sync(false);
-    // app_sink.set_callbacks(
-    //     gst_app::AppSinkCallbacks::builder()
-    //         .new_sample(move |app| {
-    //             let sample = app.pull_sample().unwrap();
-    //             let buffer = sample.buffer().unwrap();
-    //
-    //             // We don't care about buffers that are not video
-    //             if buffer
-    //                 .flags()
-    //                 .contains(gst::BufferFlags::DECODE_ONLY | gst::BufferFlags::GAP)
-    //             {
-    //                 return Ok(gst::FlowSuccess::Ok);
-    //             }
-    //
-    //             // let data = buffer.map_readable().unwrap();
-    //             // let text = std::str::from_utf8(&data).unwrap();
-    //             // println!("Subtext = {}", text);
-    //             dbg!(buffer);
-    //
-    //             Ok(gst::FlowSuccess::Ok)
-    //         })
-    //         .build(),
-    // );
+
+    let raw_audio_content = Arc::new(Mutex::new(Vec::new()));
+
+    let app_sink = pipeline
+        .by_name("sink")
+        .unwrap()
+        .downcast::<gst_app::AppSink>()
+        .unwrap();
+    app_sink.set_sync(false);
+    app_sink.set_callbacks(
+        gst_app::AppSinkCallbacks::builder()
+            .new_sample({
+                let raw_audio_content = raw_audio_content.clone();
+                move |app| {
+                    let sample = app.pull_sample().unwrap();
+                    let buffer = sample.buffer().unwrap();
+
+                    let data = buffer.map_readable().unwrap();
+
+                    let mut raw_audio_content = raw_audio_content.lock().unwrap();
+                    raw_audio_content.extend(data.to_vec());
+
+                    Ok(gst::FlowSuccess::Ok)
+                }
+            })
+            .build(),
+    );
 
     let context = glib::MainContext::default();
     let main_loop = glib::MainLoop::new(Some(&context), false);
 
     pipeline.set_state(gst::State::Playing)?;
-
 
     let bus = pipeline.bus().unwrap();
     bus.add_watch({
@@ -101,7 +87,7 @@ fn main() -> eyre::Result<()> {
             glib::Continue(true)
         }
     })
-        .expect("Failed to add bus watch");
+    .expect("Failed to add bus watch");
 
     ctrlc::set_handler({
         let pipeline_weak = pipeline.downgrade();
@@ -115,6 +101,28 @@ fn main() -> eyre::Result<()> {
     bus.remove_watch().unwrap();
 
     pipeline.set_state(gst::State::Null)?;
+
+    let mut out_wav_buffer = Cursor::new(Vec::new());
+    let mut writer = WavWriter::new(
+        &mut out_wav_buffer,
+        WavSpec {
+            channels: 1,
+            sample_rate: 48000,
+            bits_per_sample: 16,
+            sample_format: hound::SampleFormat::Int,
+        },
+    )
+    .unwrap();
+
+    let mut raw_audio_content = Cursor::new(raw_audio_content.lock().unwrap().to_vec());
+
+    while let Ok(sample) = raw_audio_content.read_i16::<LittleEndian>() {
+        writer.write_sample(sample).unwrap();
+    }
+
+    drop(writer);
+    let mut file = File::create("out.wav")?;
+    file.write_all(&out_wav_buffer.into_inner())?;
 
     Ok(())
 }
