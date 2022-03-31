398656f9f870d0d5dca3ec48d487e3d77d5618ee
Rafael Caricio <rafael@caricio.com>
2022-03-31 14:12:55 +0200

    Working for local server

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,63 @@
-use byteorder::{LittleEndian, ReadBytesExt};
 use gst::prelude::*;
 use gstreamer_app as gst_app;
-use hound::{WavSpec, WavWriter};
 use log::{debug, error, info, trace, warn};
+use serde_derive::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::{Cursor, Write};
 use std::process;
 use std::sync::{Arc, Mutex};
+use tungstenite::{connect, Message};
+use url::Url;
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Configuration {
+    config: ConfigInner,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct ConfigInner {
+    /// Sample rate the audio will be provided at.
+    sample_rate: i32,
+
+    /// Show time ranges of each word in the transcription.
+    words: bool,
+}
+
+impl Configuration {
+    pub fn new(sample_rate: i32) -> Self {
+        Self {
+            config: ConfigInner {
+                sample_rate,
+                // We always want to receive the words with their time ranges.
+                words: true,
+            },
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Transcript {
+    pub result: Vec<WordInfo>,
+    pub text: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct WordInfo {
+    #[serde(rename = "conf")]
+    pub confidence: f64,
+    pub word: String,
+    pub start: f64,
+    pub end: f64,
+}
 
 fn main() -> eyre::Result<()> {
-    pretty_env_logger::init();
+    pretty_env_logger::init_timed();
     gst::init()?;
 
     let pipeline = gst::parse_launch(
         r#"
         uridecodebin uri=file:///Users/rafael.caricio/video.mkv name=dec dec.src_1 ! audio/x-raw !
-        audioconvert ! audiorate ! audioresample ! audio/x-raw,format=S16LE,rate=48000,channels=1 ! appsink name=sink
+        audioconvert ! audiorate ! audioresample ! audio/x-raw,format=S16LE,rate=16000,channels=1 ! appsink name=sink
     "#,
     )?
         .downcast::<gst::Pipeline>()
@@ -31,7 +73,17 @@ fn main() -> eyre::Result<()> {
         debug!("Pad {} added with caps {}", name, caps_type);
     });
 
-    let raw_audio_content = Arc::new(Mutex::new(Vec::new()));
+    let (mut socket, response) = connect(Url::parse("ws://localhost:2700").unwrap())?;
+
+    let config = Configuration::new(16_000);
+    info!(
+        "config payload: {}",
+        serde_json::to_string_pretty(&config).unwrap()
+    );
+    let packet = serde_json::to_string(&config).unwrap();
+    socket.write_message(Message::Text(packet)).unwrap();
+
+    let shared_socket = Arc::new(Mutex::new(socket));
 
     let app_sink = pipeline
         .by_name("sink")
@@ -42,15 +94,39 @@ fn main() -> eyre::Result<()> {
     app_sink.set_callbacks(
         gst_app::AppSinkCallbacks::builder()
             .new_sample({
-                let raw_audio_content = raw_audio_content.clone();
+                let shared_socket = shared_socket.clone();
                 move |app| {
                     let sample = app.pull_sample().unwrap();
                     let buffer = sample.buffer().unwrap();
-
                     let data = buffer.map_readable().unwrap();
 
-                    let mut raw_audio_content = raw_audio_content.lock().unwrap();
-                    raw_audio_content.extend(data.to_vec());
+                    let mut socket = shared_socket.lock().unwrap();
+
+                    for chunk in data.chunks(8_000) {
+                        socket
+                            .write_message(Message::Binary(chunk.to_vec()))
+                            .unwrap();
+
+                        let msg = socket.read_message().unwrap();
+                        match msg {
+                            Message::Text(payload) => {
+                                match serde_json::from_str::<Transcript>(&payload) {
+                                    Ok(transcript) => {
+                                        info!(
+                                            "result: {}",
+                                            serde_json::to_string_pretty(&transcript.result)
+                                                .unwrap()
+                                        );
+                                    }
+                                    Err(_) => {
+                                        // The payload is still not a final transcript, so we just ignore it
+                                        // info!("No results...");
+                                    }
+                                }
+                            }
+                            _ => {}
+                        };
+                    }
 
                     Ok(gst::FlowSuccess::Ok)
                 }
@@ -102,27 +178,27 @@ fn main() -> eyre::Result<()> {
 
     pipeline.set_state(gst::State::Null)?;
 
-    let mut out_wav_buffer = Cursor::new(Vec::new());
-    let mut writer = WavWriter::new(
-        &mut out_wav_buffer,
-        WavSpec {
-            channels: 1,
-            sample_rate: 48000,
-            bits_per_sample: 16,
-            sample_format: hound::SampleFormat::Int,
-        },
-    )
-    .unwrap();
-
-    let mut raw_audio_content = Cursor::new(raw_audio_content.lock().unwrap().to_vec());
-
-    while let Ok(sample) = raw_audio_content.read_i16::<LittleEndian>() {
-        writer.write_sample(sample).unwrap();
-    }
-
-    drop(writer);
-    let mut file = File::create("out.wav")?;
-    file.write_all(&out_wav_buffer.into_inner())?;
+    // let mut out_wav_buffer = Cursor::new(Vec::new());
+    // let mut writer = WavWriter::new(
+    //     &mut out_wav_buffer,
+    //     WavSpec {
+    //         channels: 1,
+    //         sample_rate: 48000,
+    //         bits_per_sample: 16,
+    //         sample_format: hound::SampleFormat::Int,
+    //     },
+    // )
+    // .unwrap();
+    //
+    // let mut raw_audio_content = Cursor::new(raw_audio_content.lock().unwrap().to_vec());
+    //
+    // while let Ok(sample) = raw_audio_content.read_i16::<LittleEndian>() {
+    //     writer.write_sample(sample).unwrap();
+    // }
+    //
+    // drop(writer);
+    // let mut file = File::create("out.raw")?;
+    // file.write_all(&raw_audio_content.into_inner())?;
 
     Ok(())
 }
