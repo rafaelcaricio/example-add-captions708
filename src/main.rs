fc9707fe66ed52cd2f4960ef536b34fab3d593b1
Rafael Caricio <rafael@caricio.com>
2022-04-16 20:00:22 +0200

    Update example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use glib::translate::ToGlibPtr;
 use gst::prelude::*;
-use log::info;
+use log::{debug, info};
 use std::fs::File;
 use std::io::Write;
 use std::process;
@@ -11,9 +12,10 @@ where
     C: FnOnce() -> *mut gst_mpegts::GstMpegtsSCTESIT,
 {
     let sit = gst_sit();
+    assert!(!sit.is_null());
     unsafe {
         let section = gst_mpegts::gst_mpegts_section_from_scte_sit(sit, 500);
-        gst_mpegts::gst_mpegts_section_send_event(section, element.as_ptr());
+        gst_mpegts::gst_mpegts_section_send_event(section, element.to_glib_none().0);
         gst::ffi::gst_mini_object_unref(section as _);
     };
 }
@@ -36,6 +38,22 @@ fn send_splice_out(element: &gst::Element, event_id: u32, time: gst::ClockTime) 
     })
 }
 
+#[derive(Clone)]
+pub struct EventId(Arc<Mutex<u32>>);
+
+impl EventId {
+    pub fn new() -> Self {
+        Self(Arc::new(Mutex::new(0)))
+    }
+
+    pub fn next(&self) -> u32 {
+        let mut counter = self.0.lock().unwrap();
+        *counter += 1;
+        *counter
+    }
+}
+
+
 fn main() -> eyre::Result<()> {
     pretty_env_logger::init_timed();
     gst::init()?;
@@ -43,14 +61,17 @@ fn main() -> eyre::Result<()> {
         gst_mpegts::gst_mpegts_initialize();
     }
 
+    // ! rtpmp2tpay ! udpsink sync=true host=184.73.103.62 port=5000
     let pipeline = gst::parse_launch(
         r#"
 
+        audiotestsrc is-live=true ! audioconvert ! avenc_aac bitrate=128000 ! queue ! mux.
+
         videotestsrc is-live=true ! video/x-raw,framerate=30/1,width=1280,height=720 ! timeoverlay ! x264enc tune=zerolatency name=encoder
 
-        encoder. ! video/x-h264,profile=main ! queue ! mpegtsmux name=mux scte-35-pid=500 scte-35-null-interval=450000 alignment=7 ! rtpmp2tpay ! udpsink sync=true host=184.73.103.62 port=5000
+        encoder. ! video/x-h264,profile=main ! queue ! mpegtsmux name=mux scte-35-pid=500 scte-35-null-interval=450000
 
-        audiotestsrc is-live=true ! audioconvert ! avenc_aac bitrate=128000 ! queue ! mux.
+        mux. ! filesink sync=true location=out.ts
 
     "#,
     )?
@@ -59,12 +80,12 @@ fn main() -> eyre::Result<()> {
 
     info!("Starting pipeline...");
 
-    let ad_event_counter = Arc::new(Mutex::new(0u32));
+    let event_counter = EventId::new();
 
-    // Every 90 seconds we will loop on an ad scheduling process..
+    // Every 60 seconds we will loop on an ad scheduling process..
     glib::timeout_add(Duration::from_secs(60), {
         let pipeline_weak = pipeline.downgrade();
-        let ad_event_counter = ad_event_counter.clone();
+        let event_counter = event_counter.clone();
         move || {
             if let Some(pipeline) = pipeline_weak.upgrade() {
                 let muxer = pipeline.by_name("mux").unwrap();
@@ -73,39 +94,28 @@ fn main() -> eyre::Result<()> {
                 // is, so we use the pipeline running time to base our timing calculations
                 let now = pipeline.current_running_time().unwrap();
 
-                // How much ahead should the ad be inserted, we say 5 seconds in the future
+                // How much ahead should the ad be inserted, we say 0 seconds in the future (immediate)
                 let ahead = gst::ClockTime::from_seconds(0);
 
-                // next event id
-                let event_id = {
-                    let mut ad_event_counter = ad_event_counter.lock().unwrap();
-                    *ad_event_counter += 1;
-                    *ad_event_counter
-                };
-                send_splice_out(&muxer, event_id, now + ahead);
+                // Trigger the Splice Out event in the SCTE-35 stream
+                send_splice_out(&muxer, event_counter.next(), now + ahead);
 
                 // Now we add a timed call for the duration of the ad from now to indicate via
                 // splice in that the stream can go back to normal programming.
                 glib::timeout_add(Duration::from_secs(30), {
                     let muxer_weak = muxer.downgrade();
-                    let ad_event_counter = ad_event_counter.clone();
+                    let event_counter = event_counter.clone();
                     move || {
                         if let Some(muxer) = muxer_weak.upgrade() {
-                            // next event id
-                            let event_id = {
-                                let mut ad_event_counter = ad_event_counter.lock().unwrap();
-                                *ad_event_counter += 1;
-                                *ad_event_counter
-                            };
                             let now = muxer.current_running_time().unwrap();
-                            send_splice_in(&muxer, event_id, now + ahead);
+                            send_splice_in(&muxer, event_counter.next(), now + ahead);
                         }
-                        // This don't need to run again
+                        // This shall not run again
                         glib::Continue(false)
                     }
                 });
             }
-            // Run this again next time...
+            // Run this again after the timeout...
             glib::Continue(true)
         }
     });
@@ -118,6 +128,7 @@ fn main() -> eyre::Result<()> {
     let bus = pipeline.bus().unwrap();
     bus.add_watch({
         let main_loop = main_loop.clone();
+        let pipeline_weak = pipeline.downgrade();
         move |_, msg| {
             use gst::MessageView;
 
@@ -133,6 +144,19 @@ fn main() -> eyre::Result<()> {
                     );
                     main_loop.quit();
                 }
+                MessageView::StateChanged(s) => {
+                    if let Some(pipeline) = pipeline_weak.upgrade() {
+                        if s.src().map(|e| e == pipeline).unwrap_or(false) {
+                            debug!("Writing dot file for status: {:?}", s.current());
+
+                            let mut file = File::create(format!("Pipeline-{:?}.dot", s.current())).unwrap();
+                            let dot_data = pipeline.debug_to_dot_data(
+                                gst::DebugGraphDetails::all(),
+                            );
+                            file.write_all(dot_data.as_bytes()).unwrap();
+                        }
+                    }
+                }
                 _ => (),
             };
 
@@ -142,21 +166,9 @@ fn main() -> eyre::Result<()> {
     .expect("Failed to add bus watch");
 
     ctrlc::set_handler({
-        let pipeline_weak = pipeline.downgrade();
+        let main_loop = main_loop.clone();
         move || {
-            if let Some(pipeline) = pipeline_weak.upgrade() {
-                pipeline.call_async(|itself| {
-                    let dot_graph = itself
-                        .debug_to_dot_data(gst::DebugGraphDetails::all())
-                        .to_string();
-                    let mut graph = File::create("pipeline.dot").unwrap();
-                    graph.write_all(dot_graph.as_bytes()).unwrap();
-
-                    itself.set_state(gst::State::Null).unwrap();
-
-                    process::exit(0);
-                });
-            }
+            main_loop.quit();
         }
     })?;
 
